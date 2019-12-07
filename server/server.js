1ecce506f2dc03ddf19444cede754550c923ce1a
Natalia Malesa <nmalesa@gmail.com>
2019-12-06 20:25:07 -0600

    Server successfully connecting to client

---
+++ server/server.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.use(express.json());
+app.use(express.static('dist'));
+app.use(express.urlencoded({extended: true}));
+
+// app.get('/', (req, res) => {
+//   res.send('Hello World!');
+// });
+
+app.listen(port, () => console.log(`Listening on ${port}...`));
