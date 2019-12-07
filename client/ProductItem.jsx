83680e8dcf0d56db1d19da653a7e3cb72c1f3e33
Natalia Malesa <nmalesa@gmail.com>
2019-12-07 17:26:49 -0600

    Style fonts

--- client/ProductItem.jsx
+++ client/ProductItem.jsx
@@ -11,17 +11,35 @@ const Image = styled.img`
   object-fit: scale-down;
 `;
 
-const Text = styled.p`
+const Name = styled.p`
+  box-sizing: border-box;
+  color: rgb(0, 30, 115);
   font-family: Human BBY Web, Arial, Helvetica, sans-serif;
-  color: rgb(4, 12, 19);
+  font-size: 13px;
+  line-height: normal;
+  max-height: 48px;
+  white-space: normal;
 `;
+
+const Price = styled.p`
+  box-sizing: border-box;
+  color: rgb(29, 37, 44);
+  font-family: Human BBY Web, Arial, Helvetica, sans-serif;
+  font-size: 15px;
+  font-stretch: 100%;
+  font-style: normal;
+  font-weight: bold;
+  line-height: 18px;
+  white-space: normal;
+`;
+
 
 const ProductItem = props => (
   <React.Fragment>
     <Image src={props.productItem.image}></Image>
-    <Text>{props.productItem.name}</Text>
+    <Name>{props.productItem.name}</Name>
     <div>STARS PLACEHOLDER</div>
-    <Text><strong>{props.productItem.price}</strong></Text>
+    <Price>{props.productItem.price}</Price>
   </React.Fragment>
 );
 
